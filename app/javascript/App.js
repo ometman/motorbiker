1d5d9ee25f7c2989bcbb98e485f829909855c62b
ometman <revomet@gmail.com>
2024-04-07 23:04:47 +0000

    Fix bug in APP.js content display, removed splash and route imports

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import {
-  Routes,
-  Route,
-  BrowserRouter,
-} from 'react-router-dom';
-import Splash from './page/Splash'
 
 const App = () => (
-  <BrowserRouter>
     <h1>Welcome to MotorBiker</h1>
-    <Routes>
-      <Route path="/" element={<Splash />} />
-    </Routes>
-  </BrowserRouter>
 );
 
 export default App;
--- app/javascript/page/Splash.js
+++
@@ -1,7 +0,0 @@
-const Splash = () => (
-    render (
-        <h2>This is the Splash page</h2>
-    )
-)
-
-export default Splash;
